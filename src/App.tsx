234d2f1878e52b05b0d498b4cf5b775ef4b96878
sentic90 <princeali5549@gmail.com>
2023-06-29 15:27:30 +0200

    Fetched the games from backend added gamegrid component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
-import "./App.css";
+import GameGrid from "./components/GameGrid";
 import NavBar from "./components/NavBar";
 
 function App() {
@@ -19,7 +19,7 @@ function App() {
         </GridItem>
       </Show>
       <GridItem area="main" bg="dodgerblue">
-        Main
+        <GameGrid />
       </GridItem>
     </Grid>
   );
