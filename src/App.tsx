67c65fcbd00a8da3960e8e3bf2fde46ae80364c5
sentic90 <princeali5549@gmail.com>
2023-07-11 15:28:56 +0200

    Filtering by Platform

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,13 @@ import GenresList from "./components/GenresList";
 import NavBar from "./components/NavBar";
 import PlatformMenu from "./components/PlatformMenu";
 import { Genre } from "./hooks/useGenres";
+import { Platform } from "./hooks/usePlatforms";
 
 function App() {
   const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
+  const [selectedPlatform, setSelectedPlatform] = useState<Platform | null>(
+    null
+  );
 
   return (
     <Grid
@@ -32,8 +36,14 @@ function App() {
         </GridItem>
       </Show>
       <GridItem area="main">
-        <PlatformMenu />
-        <GameGrid selectedGenre={selectedGenre} />
+        <PlatformMenu
+          selectedPlatform={selectedPlatform}
+          onSelectPlatform={(platform) => setSelectedPlatform(platform)}
+        />
+        <GameGrid
+          selectedPlatform={selectedPlatform}
+          selectedGenre={selectedGenre}
+        />
       </GridItem>
     </Grid>
   );
