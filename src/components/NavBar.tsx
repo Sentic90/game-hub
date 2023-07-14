53bb055981bac521c6f8b746feaf3618620bd39e
sentic90 <princeali5549@gmail.com>
2023-07-14 18:47:24 +0200

    added search bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,13 +2,15 @@ import { HStack, Image, Text } from "@chakra-ui/react";
 
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "../ColorModeSwitch";
+import SearchInput from "./SearchInput";
 const NavBar = () => {
   return (
-    <HStack justifyContent="space-between" padding="10px">
+    <HStack padding="10px">
       <Image src={logo} boxSize="60px" />
-      <Text fontSize="5xl" as="b">
+      <Text fontSize="4xl" as="b">
         GameHub
       </Text>
+      <SearchInput />
       <ColorModeSwitch />
     </HStack>
   );
--- src/components/PlatformMenu.tsx
+++ src/components/PlatformMenu.tsx
@@ -1,4 +1,11 @@
-import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+import {
+  Button,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Spinner,
+} from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
 import usePlatforms, { Platform } from "../hooks/usePlatforms";
 
@@ -7,9 +14,22 @@ interface Props {
   selectedPlatform: Platform | null;
 }
 const PlatformMenu = ({ selectedPlatform, onSelectPlatform }: Props) => {
-  const { data, error } = usePlatforms();
+  const { data, error, isLoading } = usePlatforms();
 
   if (error) return null;
+  if (isLoading)
+    return (
+      <Menu>
+        <MenuButton as={Button} rightIcon={<BsChevronDown />}>
+          {selectedPlatform?.name || "Platforms"}
+        </MenuButton>
+        <MenuList>
+          <MenuItem>
+            <Spinner />
+          </MenuItem>
+        </MenuList>
+      </Menu>
+    );
   return (
     <Menu>
       <MenuButton as={Button} rightIcon={<BsChevronDown />}>
